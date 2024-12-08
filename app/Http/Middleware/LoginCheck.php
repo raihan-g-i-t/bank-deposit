26b2985295e5f1ad183003ab441e959b08b3079b
raihan133 <opu.raihankhan@gmail.com>
2024-12-08 22:28:26 +0600

    message

--- app/Http/Middleware/LoginCheck.php
+++ app/Http/Middleware/LoginCheck.php
@@ -23,7 +23,7 @@ class LoginCheck
                     return $next($request);
                 }
             }else{
-                return redirect()->route('login');
+                return redirect()->route('login')->with('data', 'Login First');
             }
     }
 }
